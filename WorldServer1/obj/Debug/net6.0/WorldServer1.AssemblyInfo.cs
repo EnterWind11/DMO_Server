fa2a1ce0515cac868c82995ce9c4d2b043e6ad14
EnterWind11 <kaitoaokage@gmail.com>
2024-05-29 00:10:22 +0700

    Update

--- WorldServer1/obj/Debug/net6.0/WorldServer1.AssemblyInfo.cs
+++ WorldServer1/obj/Debug/net6.0/WorldServer1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldServer1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02801f01d7f535cc5c7cec4463bd0c1691b70b07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3f46d8343125d4add0dfaade846eba6072f45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldServer1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldServer1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
